c1d32711be75da879666f512dfacb4ffdb901aaf
Meitar KESHET <mkeshet@e2r4p4.42.fr>
2020-09-17 15:47:06 +0200

    ex 00 before norm

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkeshet <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/17 14:30:15 by mkeshet           #+#    #+#             */
+/*   Updated: 2020/09/17 15:45:51 by mkeshet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int ft_iterative_factorial(int nb)
+{
+	int i;
+	int facto;
+
+	i = 1;
+	facto = nb;
+	if (nb < 0)
+		return (0);
+	if (nb == 0)
+		return (1);
+	while (nb > i)
+	{
+		facto *= i;
+		i++;
+	}
+	return (facto);
+}
+
+int		main(void)
+{
+	int nb = 0;
+	printf("%d", ft_iterative_factorial(nb));
+
+	return (0);
+}
